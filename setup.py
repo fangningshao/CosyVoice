9b05b07d31bcb6d7f7db769c71e9c03491790770
Fangning Shao <fangning.shao@gmail.com>
2025-05-12 01:13:14 +0800

    Add test transcripts for cloing test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+import os
+
+requirements=[]
+
+setup(
+    name='cosyvoice',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=requirements,
+)
